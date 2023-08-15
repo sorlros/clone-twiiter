a938f926b603dce4f81dd03d77127d52fb9a2f6e
sorlros <sorlros2047@gmail.com>
2023-08-15 16:13:00 +0900

    follow & unfollow

---
+++ components/posts/PostItem.tsx
@@ -0,0 +1,92 @@
+import { useRouter } from "next/router";
+import useLoginModal from "@/hooks/useLoginModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
+import { useCallback, useMemo } from "react";
+import { formatDistanceToNowStrict } from "date-fns";
+import Avatar from "../Avatar";
+import { AiOutlineHeart, AiOutlineMessage } from "react-icons/ai";
+
+interface PostItemProps {
+	data: Record<string, any>;
+	userId?: string;
+}
+
+const PostItem: React.FC<PostItemProps> = ({ data, userId }) => {
+	const router = useRouter();
+	const loginModal = useLoginModal();
+
+	const { data: currentUser } = useCurrentUser();
+
+	const goToUser = useCallback(
+		(event: any) => {
+			event.stopPropagation();
+
+			router.push(`/users/${data.user.id}`);
+		},
+		[router, data.user.id]
+	);
+
+	const goToPost = useCallback(() => {
+		router.push(`/post/${data.id}`);
+	}, [router, data.id]);
+
+	const onLike = useCallback(
+		(event: any) => {
+			event.stopPropagation();
+
+			loginModal.onOpen();
+		},
+		[loginModal]
+	);
+
+	const createdAt = useMemo(() => {
+		if (!data?.createdAt) {
+			return null;
+		}
+
+		return formatDistanceToNowStrict(new Date(data.createdAt));
+	}, [data?.createdAt]);
+	return (
+		<div
+			onClick={goToPost}
+			className="border-b-[1px] border-neutral-800 p-5 curser-pointer hover:bg-neutral-900 transition"
+		>
+			<div className="flex flex-row items-start gap-3">
+				<Avatar userId={data.user.id} />
+				<div>
+					<div className="flex flex-row items-center gap-2">
+						<p
+							className="text-white font-semibold cursor-pointer hover:underline"
+							onClick={goToUser}
+						>
+							{data.user.name}
+						</p>
+						<span
+							className="text-neutral-500 cursor-pointer hover:underline hidden md:block"
+							onClick={goToUser}
+						>
+							@{data.user.username}
+						</span>
+						<span className="text-neutral-500 text-sm">{createdAt}</span>
+					</div>
+					<div className="text-white mt-1">{data.body}</div>
+					<div className="flex flex-row items-center mt-3 gap-10">
+						<div className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-sky-500">
+							<AiOutlineMessage size={20} />
+							<p>{data.comments?.length || 0}</p>
+						</div>
+						<div
+							className="flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500"
+							onClick={onLike}
+						>
+							<AiOutlineHeart size={20} />
+							<p>{data.comments?.length || 0}</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default PostItem;
--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -6,6 +6,7 @@ import { format } from "date-fns";
 import Button from "../Button";
 import { BiCalendar } from "react-icons/bi";
 import useEditModal from "@/hooks/useEditModal";
+import useFollow from "@/hooks/useFollow";
 
 interface UserBioProps {
 	userId: string;
@@ -16,6 +17,8 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 	const { data: fetchedUser } = useUser(userId);
 
 	const editModal = useEditModal();
+
+	const { isFollowing, toggleFollow } = useFollow(userId);
 
 	const createdAt = useMemo(() => {
 		if (!fetchedUser?.createdAt) {
@@ -30,7 +33,12 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 				{currentUser?.id === userId ? (
 					<Button secondary label="Edit" onClick={editModal.onOpen} />
 				) : (
-					<Button onClick={() => {}} label="Follow" secondary />
+					<Button
+						onClick={toggleFollow}
+						label={isFollowing ? "Unfollow" : "Follow"}
+						secondary={!isFollowing}
+						outline={isFollowing}
+					/>
 				)}
 			</div>
 			<div className="mt-7 px-4">
